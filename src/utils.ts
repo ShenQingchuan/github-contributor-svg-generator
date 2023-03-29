23ae98a7f719129454495c3232d8c1dae44a61ea
Simon He <13917107469@163.com>
2023-03-29 09:33:34 +0800

    chore: update

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,11 @@
 import child_process from 'child_process'
 import fsp from 'fs/promises'
 import path from 'path'
-import { fileURLToPath } from 'url';
 
 export function getDefaultValue(name:string){
  return child_process.spawnSync(`echo $${name}`,{shell:true,encoding:'utf-8',stdio:'pipe'}).stdout.replace('\n','')
 }
 
-const root = process.cwd()
 export async function getPkgName(){
   try {
     return JSON.parse(await fsp.readFile(path.resolve(process.cwd(),'./package.json'),'utf-8')).name
